6212881705f38a9fdbcc8d0d96b5d40d5030a57e
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-15 20:23:06 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,29 @@
+require 'pry' 
+
+class CashRegister 
+  
+attr_accessor :discounts, :add_items, :inventory, :void  
+  
+def initialize(employee_discount) 
+  @total = 0  
+  @employee_discount = 20
+end
+
+def total 
+  return @total
+end
+
+def add_items 
+end
+
+
+def apply_discount(employee_discount)  
+  
+end 
+
+def items    
+end
+
+end
+
 
